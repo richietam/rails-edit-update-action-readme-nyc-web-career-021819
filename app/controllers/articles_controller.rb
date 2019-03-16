77bcde67d5ecfa845ccc9c131e628a27d95eea3c
richietam <richardtam.ny@gmail.com>
2019-03-16 10:24:25 -0400

    finished lab

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
@@ -11,11 +12,22 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+
   def create
     @article = Article.new
     @article.title = params[:title]
     @article.description = params[:description]
     @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], description: params[:description])
     redirect_to article_path(@article)
   end
 
